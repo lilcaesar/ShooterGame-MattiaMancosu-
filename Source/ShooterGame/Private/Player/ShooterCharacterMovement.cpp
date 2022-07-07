f71597585b38067d7561ac27278a6e1ebdd83623
Mattia Mancosu <lilcaesar4@gmail.com>
2022-07-07 18:24:12 +0200

    Creted UShooterSavedMove_Character and overrided some first CharacterMovementComponent functions
    TODO perform movement inside UShooterCharacterMovement::OnMovementUpdated

--- Source/ShooterGame/Private/Player/ShooterCharacterMovement.cpp
+++ Source/ShooterGame/Private/Player/ShooterCharacterMovement.cpp
@@ -31,3 +31,61 @@ float UShooterCharacterMovement::GetMaxSpeed() const
 
 	return MaxSpeed;
 }
+
+FSavedMovePtr UShooterCharacterMovement::AllocateNewMove()
+{
+	
+	return FSavedMovePtr(new UShooterSavedMove_Character());
+}
+
+bool UShooterCharacterMovement::DoTeleport(bool bReplayingMoves)
+{
+	if ( CharacterOwner)
+	{
+		bIsPressingTeleport = true;
+		return true;
+	}
+	
+	return false;
+}
+
+void UShooterCharacterMovement::UpdateFromCompressedFlags(uint8 Flags)
+{
+	Super::UpdateFromCompressedFlags(Flags);
+	bIsPressingTeleport = ((Flags & UShooterSavedMove_Character::FLAG_Custom_0) != 0);
+}
+
+void UShooterCharacterMovement::OnMovementUpdated(float DeltaSeconds, const FVector& OldLocation,
+	const FVector& OldVelocity)
+{
+	Super::OnMovementUpdated(DeltaSeconds, OldLocation, OldVelocity);
+	
+	if(bIsPressingTeleport)
+	{
+		//TODO perform movement
+		bIsPressingTeleport = false;
+	}
+}
+
+uint8 UShooterSavedMove_Character::GetCompressedFlags() const
+{
+	uint8 Result = 0;
+	//Preserving original implementation
+	if (bPressedJump)
+	{
+		Result |= FLAG_JumpPressed;
+	}
+
+	if (bWantsToCrouch)
+	{
+		Result |= FLAG_WantsToCrouch;
+	}
+
+	//Adding custom flags
+	if(bPressedTeleport)
+	{
+		Result |= FLAG_Custom_0;
+	}
+
+	return Result;
+}
--- Source/ShooterGame/Public/Player/ShooterCharacterMovement.h
+++ Source/ShooterGame/Public/Player/ShooterCharacterMovement.h
@@ -13,5 +13,27 @@ class UShooterCharacterMovement : public UCharacterMovementComponent
 	GENERATED_UCLASS_BODY()
 
 	virtual float GetMaxSpeed() const override;
+
+	virtual FSavedMovePtr AllocateNewMove();
+	/**
+	* Perform Teleport. Called by Character when a teleport has been detected because ShooterCharacter->bPressedTeleport was true. Checks ShooterCharacter->CanTeleport().
+	* @param	bReplayingMoves: true if this is being done as part of replaying moves on a locally controlled client after a server correction.
+	* @return	True if the teleport was triggered successfully.
+	*/
+	virtual bool DoTeleport(bool bReplayingMoves);
+	
+protected:
+	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
+	virtual void OnMovementUpdated(float DeltaSeconds, const FVector& OldLocation, const FVector& OldVelocity) override;
+
+	/* True if teleport button is been pressed */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Teleport")
+	bool bIsPressingTeleport;
 };
 
+class UShooterSavedMove_Character : public FSavedMove_Character
+{
+	virtual uint8 GetCompressedFlags() const override;
+	
+	uint32 bPressedTeleport:1;
+};
