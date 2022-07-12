905105763b0a213cbc0e9bfeb6f766355b7649fc
Mattia Mancosu <lilcaesar4@gmail.com>
2022-07-12 23:28:01 +0200

    Added Pickup class for the weapons
    TODO add a spawn function to the character when it dies and handle networking

---
+++ Source/ShooterGame/Private/Pickups/ShooterPickup_WeaponWithAmmo.cpp
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShooterPickup_WeaponWithAmmo.h"
+#include "Weapons/ShooterWeapon.h"
+#include "OnlineSubsystemUtils.h"
+
+AShooterPickup_WeaponWithAmmo::AShooterPickup_WeaponWithAmmo(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	AmmoClips = 0;
+	RemainingAmmoInClip = 0;
+}
+
+AShooterPickup_WeaponWithAmmo::AShooterPickup_WeaponWithAmmo(const FObjectInitializer& ObjectInitializer, int32 Clips, int32 RemainingAmmo) : Super(ObjectInitializer), RemainingAmmoInClip(RemainingAmmo)
+{
+	AmmoClips = Clips;
+}
+
+void AShooterPickup_WeaponWithAmmo::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	SelfDestructTimer -= DeltaTime;
+	if(SelfDestructTimer<0)
+	{
+		Destroy();
+	}
+}
+
+void AShooterPickup_WeaponWithAmmo::GivePickupTo(AShooterCharacter* Pawn)
+{
+	AShooterWeapon* Weapon = (Pawn ? Pawn->FindWeapon(WeaponType) : NULL);
+	if (Weapon)
+	{
+		//Adding RemainingAmmoInClip to the original implementation
+		int32 Qty = AmmoClips * Weapon->GetAmmoPerClip() + RemainingAmmoInClip;
+		Weapon->GiveAmmo(Qty);
+
+		// Fire event for collected ammo
+		if (Pawn)
+		{
+			const UWorld* World = GetWorld();
+			const IOnlineEventsPtr Events = Online::GetEventsInterface(World);
+			const IOnlineIdentityPtr Identity = Online::GetIdentityInterface(World);
+
+			if (Events.IsValid() && Identity.IsValid())
+			{							
+				AShooterPlayerController* PC = Cast<AShooterPlayerController>(Pawn->Controller);
+				if (PC)
+				{
+					ULocalPlayer* LocalPlayer = Cast<ULocalPlayer>(PC->Player);
+
+					if (LocalPlayer)
+					{
+						const int32 UserIndex = LocalPlayer->GetControllerId();
+						TSharedPtr<const FUniqueNetId> UniqueID = Identity->GetUniquePlayerId(UserIndex);			
+						if (UniqueID.IsValid())
+						{
+							FVector Location = Pawn->GetActorLocation();
+
+							FOnlineEventParms Params;		
+
+							Params.Add( TEXT( "SectionId" ), FVariantData( (int32)0 ) ); // unused
+							Params.Add( TEXT( "GameplayModeId" ), FVariantData( (int32)1 ) ); // @todo determine game mode (ffa v tdm)
+							Params.Add( TEXT( "DifficultyLevelId" ), FVariantData( (int32)0 ) ); // unused
+
+							Params.Add( TEXT( "ItemId" ), FVariantData( (int32)Weapon->GetAmmoType() + 1 ) ); // @todo come up with a better way to determine item id, currently health is 0 and ammo counts from 1
+							Params.Add( TEXT( "AcquisitionMethodId" ), FVariantData( (int32)0 ) ); // unused
+							Params.Add( TEXT( "LocationX" ), FVariantData( Location.X ) );
+							Params.Add( TEXT( "LocationY" ), FVariantData( Location.Y ) );
+							Params.Add( TEXT( "LocationZ" ), FVariantData( Location.Z ) );
+							Params.Add( TEXT( "ItemQty" ), FVariantData( (int32)Qty ) );		
+
+							Events->TriggerEvent(*UniqueID, TEXT("CollectPowerup"), Params);
+						}
+					}
+				}
+			}
+		}
+	}
+	//Once picked it cannot respawn and should be destroyed
+	Destroy();
+}
---
+++ Source/ShooterGame/Public/Pickups/ShooterPickup_WeaponWithAmmo.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ShooterPickup_Ammo.h"
+#include "ShooterPickup_WeaponWithAmmo.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class SHOOTERGAME_API AShooterPickup_WeaponWithAmmo : public AShooterPickup_Ammo
+{
+	GENERATED_BODY()
+public:
+	AShooterPickup_WeaponWithAmmo(const FObjectInitializer& ObjectInitializer);
+	AShooterPickup_WeaponWithAmmo(const FObjectInitializer& ObjectInitializer, int32 Clips, int32 RemainingAmmo);
+	
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category=Pickup)
+	int32 RemainingAmmoInClip;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Pickup)
+	float SelfDestructTimer = 10;
+	
+	/** give pickup */
+	virtual void GivePickupTo(AShooterCharacter* Pawn) override;
+};
